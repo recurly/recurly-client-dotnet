3647b35833fe21030e3483f148ad6442f96b0ede
Patrick Hyatt <patrick.hyatt@gmail.com>
2012-02-22 11:17:49 -0500

    -Addition of missing files....

---
+++ Library/Configuration/RecurlyApplication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+namespace Recurly.Configuration
+{
+    public class RecurlyApplication : ConfigurationElement
+    {
+        [ConfigurationProperty("name", IsRequired = true)]
+        public string Name { get { return this["name"] as string; } }
+
+        [ConfigurationProperty("apikey", IsRequired = true)]
+        public string ApiKey { get { return this["apikey"] as string; } }
+
+        [ConfigurationProperty("privatekey", IsRequired = true)]
+        public string PrivateKey { get { return this["privatekey"] as string; } }
+
+        [ConfigurationProperty("subdomain", IsRequired = true)]
+        public string Subdomain { get { return this["subdomain"] as string; } }
+    }
+}
---
+++ Library/Configuration/RecurlyApplicationCollection.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace Recurly.Configuration
+{
+    public class RecurlyApplicationCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new RecurlyApplication();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((RecurlyApplication)element).Name;
+        }
+
+        public new RecurlyApplication this[string appName]
+        {
+            get { return this.BaseGet(appName) as RecurlyApplication; }
+        }
+    }
+}
