7c1f8888b45f0e06fe714d05a80ba9e2c5341a3c
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2014-04-14 19:00:09 -0700

    add missing file

---
+++ Library/List/RefundList.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Recurly
+{
+    internal class RefundList : IEnumerable<Refund>
+    {
+        private List<Refund> Refunds = new List<Refund>();
+  
+        internal RefundList(IEnumerable<Adjustment> adjustments, bool prorate, int quantity = 0)
+        {
+            foreach (var adjustment in adjustments)
+            {
+                var count = quantity == 0
+                    ? adjustment.Quantity
+                    : quantity;
+
+                var refund = new Refund(adjustment, prorate, count);
+                Refunds.Add(refund);
+            }
+        }
+
+        internal void WriteXml(XmlTextWriter writer) 
+        {
+            writer.WriteStartElement("invoice");
+            writer.WriteStartElement("line_items");
+
+            foreach (var refund in Refunds)
+            {
+                refund.WriteXml(writer);
+            }
+
+            writer.WriteEndElement(); // line_items
+            writer.WriteEndElement(); // invoice
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Refunds.GetEnumerator();
+        }
+
+        public IEnumerator<Refund> GetEnumerator()
+        {
+            return Refunds.GetEnumerator();
+        }
+    }
+}
