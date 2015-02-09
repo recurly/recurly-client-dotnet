38e2af5838efb7b9b659deabbbd6f766882af68d
shawnspeak <shawnb@xcarab.com>
2015-02-09 08:10:30 -0800

    Added support for previewing subscription changes

--- Library/Adjustment.cs
+++ Library/Adjustment.cs
@@ -187,7 +187,9 @@ namespace Recurly
                         break;
 
                     case "created_at":
-                        CreatedAt = reader.ReadElementContentAsDateTime();
+                        DateTime createdAt;
+                        if (DateTime.TryParse(reader.ReadElementContentAsString(), out createdAt))
+                            CreatedAt = createdAt;                        
                         break;
 
                     case "state":
