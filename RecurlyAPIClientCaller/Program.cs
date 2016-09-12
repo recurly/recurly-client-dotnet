77edbc95ca7c14e456388747c251dd52d3a54ad5
Juan Carlos Valladolid <juancarlos.valladolid@veinteractive.com>
2016-09-12 16:24:12 +0200

    Adding the classes to use the UsageRecord endpoints

---
+++ RecurlyAPIClientCaller/Program.cs
@@ -0,0 +1,28 @@
+﻿using Recurly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecurlyAPIClientCaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsageRecord usageRecord = new UsageRecord();
+
+            usageRecord.SubscriptionUuid = "3849d9c3bf6fefdd9ed16d4224b0e8a9";
+            usageRecord.AddOnCode = "TransactionAmountAddOn";
+
+            usageRecord.Amount = 1000;
+            usageRecord.MerchantTag = "OrderID: 111-222-333-444";
+            usageRecord.RecordingTimestamp = DateTime.Now.AddHours(-3);
+            usageRecord.UsageTimestamp = DateTime.Now;
+
+            UsageRecord usageRecorded = usageRecord.Log();
+
+        }
+    }
+}
---
+++ Test/UsageRecordTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FluentAssertions;
+
+
+namespace Recurly.Test
+{
+    public class UsageRecordTest : BaseTest
+    {
+        [RecurlyFact(TestEnvironment.Type.Integration)]
+        public void LogUsageRecord()
+        {
+            var usageRecord = new UsageRecord();
+
+            usageRecord.SubscriptionUuid = "3873a1b40b9568d1defca74b249139e4";
+            usageRecord.AddOnCode = "TransactionAmountAddOn";
+
+            usageRecord.Amount = 1000;
+            usageRecord.MerchantTag = "OrderID: 111-222-333-444";
+            usageRecord.RecordingTimestamp = DateTime.Now.AddHours(-3);
+            usageRecord.UsageTimestamp = DateTime.Now;
+
+            UsageRecord usageRecorded = usageRecord.Log();
+
+            usageRecorded.Should().NotBeNull();
+        }
+    }
+}
