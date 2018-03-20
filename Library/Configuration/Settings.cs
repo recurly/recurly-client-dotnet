e92a11686632b0886fec078333b0f48be37a647e
benjamin eckel <ben@recurly.com>
2018-03-20 13:35:00 -0500

    Bump API v2.11

--- Library/Configuration/Settings.cs
+++ Library/Configuration/Settings.cs
@@ -63,7 +63,7 @@ namespace Recurly.Configuration
         }
 
         protected const string RecurlyServerUri = "https://{0}.recurly.com/v2{1}";
-        public const string RecurlyApiVersion = "2.10";
+        public const string RecurlyApiVersion = "2.11";
 
         // static, unlikely to change
         public string UserAgent
