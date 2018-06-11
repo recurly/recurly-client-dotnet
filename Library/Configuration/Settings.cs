43d50d9205f3df727d3aa4a4f93efe523b93995b
benjamin eckel <ben@recurly.com>
2018-06-11 13:04:21 -0500

    Bump to API v2.13

--- Library/Configuration/Settings.cs
+++ Library/Configuration/Settings.cs
@@ -63,7 +63,7 @@ namespace Recurly.Configuration
         }
 
         protected const string RecurlyServerUri = "https://{0}.recurly.com/v2{1}";
-        public const string RecurlyApiVersion = "2.12";
+        public const string RecurlyApiVersion = "2.13";
         public const string ValidDomain = ".recurly.com";
 
         // static, unlikely to change
