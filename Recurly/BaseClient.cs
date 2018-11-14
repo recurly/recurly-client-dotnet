9473b371dff7a6521084f1300478e361d6b1d5c8
Benjamin Eckel <bhelx@simst.im>
2018-11-14 14:04:30 -0600

    match to string

--- Recurly/BaseClient.cs
+++ Recurly/BaseClient.cs
@@ -76,7 +76,7 @@ namespace Recurly {
 
         protected string InterpolatePath(string path, Dictionary<string, object> urlParams) {
           var regex = new Regex("{(.*)}");
-          return regex.Replace(path, m => urlParams[m.Value]);
+          return regex.Replace(path, m => urlParams[m.Value.ToString()]);
         }
 
         protected string ApiVersion() {
