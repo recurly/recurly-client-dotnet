53b18abc52573ba6b5c900dc201e9c6a615656ef
Benjamin Eckel <bhelx@simst.im>
2018-11-14 13:57:05 -0600

    use regex

--- Recurly/BaseClient.cs
+++ Recurly/BaseClient.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Linq;
 using RestSharp;
 using RestSharp.Authenticators;
