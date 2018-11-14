b6fd42f8b553d6234d89c4fe01869364c1af7fc5
Benjamin Eckel <bhelx@simst.im>
2018-11-14 16:37:33 -0600

    update program, remove objs

--- Recurly.TestRig/Program.cs
+++ Recurly.TestRig/Program.cs
@@ -11,26 +11,38 @@ namespace RecurlyTestRig
           try {
             var subdomain = Environment.GetEnvironmentVariable("RECURLY_SUBDOMAIN");
             var apiKey = Environment.GetEnvironmentVariable("RECURLY_API_KEY");
-            var client = new Recurly.Client(subdomain, apiKey);
+            var client = new Recurly.Client(subdomain, apiKey);;
 
-            var account = client.GetAccount("code-benjamin-du-monde");
-            Console.WriteLine(account.CreatedAt);
+            var address = new Address() {
+                Street1 = "123 Main St",
+                City = "New Orleans",
+                Region = "LA",
+                PostalCode = "70114",
+                Country = "US",
+            };
 
-            var createAccount = new AccountCreate() {
+            var account = new AccountCreate() {
                 Code = Guid.NewGuid().ToString(),
                 Username = "myuser",
-                Address = new Address() {
-                  Street1 = "123 Main St",
-                  City = "New Orleans",
-                  Region = "LA",
-                  PostalCode = "70114",
-                  Country = "US",
+                Address = address,
+                BillingInfo = new BillingInfoCreate() {
+                    FirstName = "Benjamin",
+                    LastName = "Eckel",
+                    Number = "4111-1111-1111-1111",
+                    Month = "12",
+                    Year = "2022",
+                    Address = address,
                 }
             };
 
-            var createdAccount = client.CreateAccount(createAccount);
-            Console.WriteLine(createdAccount.CreatedAt);
-            Console.WriteLine(createdAccount); 
+            var subscriptionRequest = new SubscriptionCreate() {
+                Account = account,
+                PlanCode = "gold1",
+                Currency = "USD",
+            };
+
+            var subscription = client.CreateSubscription(subscriptionRequest);
+            Console.WriteLine(subscription); 
 
             try {
                 var nonexistentAccount = client.GetAccount("idontexist");
