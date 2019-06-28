1c1dec51b6dd2d239a997a1fa49c75aa4cf375a2
Benjamin Eckel <bhelx@simst.im>
2019-06-28 05:31:23 +0000

    Fri Jun 28 05:31:22 UTC 2019 Upgrade API version v2018-08-09

---
+++ Recurly/Resources/AccountPurchase.cs
@@ -0,0 +1,95 @@
+/**
+ * This file is automatically created by Recurly's OpenAPI generation process
+ * and thus any edits you make by hand will be lost. If you wish to make a
+ * change to this file, please create a Github issue explaining the changes you
+ * need and we will usher them to the appropriate places.
+ */
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Newtonsoft.Json;
+
+namespace Recurly.Resources
+{
+    [ExcludeFromCodeCoverage]
+    public class AccountPurchase : Request
+    {
+
+
+        [JsonProperty("acquisition")]
+        public AccountAcquisitionUpdatable Acquisition { get; set; }
+
+
+        [JsonProperty("address")]
+        public Address Address { get; set; }
+
+        /// <value>An enumerable describing the billing behavior of the account, specifically whether the account is self-paying or will rely on the parent account to pay.</value>
+        [JsonProperty("bill_to")]
+        public string BillTo { get; set; }
+
+
+        [JsonProperty("billing_info")]
+        public BillingInfoCreate BillingInfo { get; set; }
+
+        /// <value>Additional email address that should receive account correspondence. These should be separated only by commas. These CC emails will receive all emails that the `email` field also receives.</value>
+        [JsonProperty("cc_emails")]
+        public string CcEmails { get; set; }
+
+        /// <value>The unique identifier of the account. This cannot be changed once the account is created.</value>
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+
+        [JsonProperty("company")]
+        public string Company { get; set; }
+
+
+        [JsonProperty("custom_fields")]
+        public List<CustomField> CustomFields { get; set; }
+
+        /// <value>The email address used for communicating with this customer. The customer will also use this email address to log into your hosted account management pages. This value does not need to be unique.</value>
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        /// <value>The tax exemption certificate number for the account. If the merchant has an integration for the Vertex tax provider, this optional value will be sent in any tax calculation requests for the account.</value>
+        [JsonProperty("exemption_certificate")]
+        public string ExemptionCertificate { get; set; }
+
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        /// <value>Optional, but if present allows an existing account to be used and updated as part of the purchase.</value>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        /// <value>The account code of the parent account to be associated with this account. Passing an empty value removes any existing parent association from this account. If both `parent_account_code` and `parent_account_id` are passed, the non-blank value in `parent_account_id` will be used. Only one level of parent child relationship is allowed. You cannot assign a parent account that itself has a parent account.</value>
+        [JsonProperty("parent_account_code")]
+        public string ParentAccountCode { get; set; }
+
+        /// <value>The UUID of the parent account to be associated with this account. Passing an empty value removes any existing parent association from this account. If both `parent_account_code` and `parent_account_id` are passed, the non-blank value in `parent_account_id` will be used. Only one level of parent child relationship is allowed. You cannot assign a parent account that itself has a parent account.</value>
+        [JsonProperty("parent_account_id")]
+        public string ParentAccountId { get; set; }
+
+        /// <value>Used to determine the language and locale of emails sent on behalf of the merchant to the customer. The list of locales is restricted to those the merchant has enabled on the site.</value>
+        [JsonProperty("preferred_locale")]
+        public string PreferredLocale { get; set; }
+
+        /// <value>The tax status of the account. `true` exempts tax on the account, `false` applies tax on the account.</value>
+        [JsonProperty("tax_exempt")]
+        public bool? TaxExempt { get; set; }
+
+        /// <value>A secondary value for the account.</value>
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        /// <value>The VAT number of the account (to avoid having the VAT applied). This is only used for manually collected invoices.</value>
+        [JsonProperty("vat_number")]
+        public string VatNumber { get; set; }
+
+    }
+}
---
+++ Recurly/Resources/SubscriptionShippingPurchase.cs
@@ -0,0 +1,31 @@
+/**
+ * This file is automatically created by Recurly's OpenAPI generation process
+ * and thus any edits you make by hand will be lost. If you wish to make a
+ * change to this file, please create a Github issue explaining the changes you
+ * need and we will usher them to the appropriate places.
+ */
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Newtonsoft.Json;
+
+namespace Recurly.Resources
+{
+    [ExcludeFromCodeCoverage]
+    public class SubscriptionShippingPurchase : Request
+    {
+
+        /// <value>Assigns the subscription's shipping cost. If this is greater than zero then a `method_id` or `method_code` is required.</value>
+        [JsonProperty("amount")]
+        public float? Amount { get; set; }
+
+        /// <value>The code of the shipping method used to deliver the subscription. If `method_id` and `method_code` are both present, `method_id` will be used.</value>
+        [JsonProperty("method_code")]
+        public string MethodCode { get; set; }
+
+        /// <value>The id of the shipping method used to deliver the subscription. If `method_id` and `method_code` are both present, `method_id` will be used.</value>
+        [JsonProperty("method_id")]
+        public string MethodId { get; set; }
+
+    }
+}
