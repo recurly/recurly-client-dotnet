c46d693cc0d88210c8841b76680376fe79e12dc4
Joanna Sese <jmgsese@gmail.com>
2020-04-08 16:35:10 -0500

    Refactor and update API version

--- Library/Configuration/Settings.cs
+++ Library/Configuration/Settings.cs
@@ -78,7 +78,7 @@ namespace Recurly.Configuration
         }
 
         protected const string RecurlyServerUri = "https://{0}.recurly.com/v2{1}";
-        public const string RecurlyApiVersion = "2.25";
+        public const string RecurlyApiVersion = "2.26";
         public const string ValidDomain = ".recurly.com";
 
         // static, unlikely to change
