0396b1709283201d849d3f97ed354680ce0f14ad
Doug Miller <doug@douglas-miller.com>
2020-04-27 16:48:55 -0500

    Refactor tests

--- Recurly.Tests/BaseClientTest.cs
+++ Recurly.Tests/BaseClientTest.cs
@@ -67,7 +67,7 @@ namespace Recurly.Tests
         [Fact]
         public void CanProperlyFetchAResource()
         {
-            var client = this.GetResourceSuccessClient();
+            var client = this.MockResourceClient(SuccessResponse(System.Net.HttpStatusCode.OK));
             MyResource resource = client.GetResource("benjamin", "param1", new DateTime(2020, 01, 01));
             Assert.Equal("benjamin", resource.MyString);
         }
@@ -75,7 +75,7 @@ namespace Recurly.Tests
         [Fact]
         public async void CanProperlyFetchAResourceAsync()
         {
-            var client = this.GetResourceSuccessClient();
+            var client = this.MockResourceClient(SuccessResponse(System.Net.HttpStatusCode.OK));
             MyResource resource = await client.GetResourceAsync("benjamin", "param1", new DateTime(2020, 01, 01));
             Assert.Equal("benjamin", resource.MyString);
         }
@@ -83,7 +83,7 @@ namespace Recurly.Tests
         [Fact]
         public void CanProperlyCreateAResource()
         {
-            var client = this.CreateResourceSuccessClient();
+            var client = this.MockResourceClient(SuccessResponse(System.Net.HttpStatusCode.Created));
             var request = new MyResourceCreate()
             {
                 MyString = "benjamin"
@@ -103,45 +103,33 @@ namespace Recurly.Tests
         [Fact]
         public void WillThrowNotFoundExceptionForNon200()
         {
-            var client = this.GetResourceFailureClient();
+            var client = this.MockResourceClient(NotFoundResponse());
             Assert.Throws<Recurly.Errors.NotFound>(() => client.GetResource("benjamin", "param1", new DateTime(2020, 01, 01)));
         }
 
         [Fact]
         public void WillThrowAnExceptionWhenResponseHasErrorException()
         {
-            var client = this.GetErroredResponseClient();
+            var client = this.MockResourceClient(ErroredResponse());
             Assert.Throws<Recurly.RecurlyError>(() => client.GetResource("benjamin", "param1", new DateTime(2020, 01, 01)));
         }
 
-        private MyClient GetResourceSuccessClient()
+        private Mock<IRestResponse<MyResource>> SuccessResponse(System.Net.HttpStatusCode status)
         {
             var data = new MyResource()
             {
                 MyString = "benjamin"
             };
             var response = new Mock<IRestResponse<MyResource>>();
-            response.Setup(_ => _.StatusCode).Returns(System.Net.HttpStatusCode.OK);
+            response.Setup(_ => _.StatusCode).Returns(status);
             response.Setup(_ => _.Content).Returns("{\"my_string\": \"benjamin\"}");
             response.Setup(_ => _.Headers).Returns(new List<Parameter> { });
             response.Setup(_ => _.Data).Returns(data);
 
-            var mockIRestClient = new Mock<IRestClient>();
-            mockIRestClient
-                .Setup(x => x.Execute<MyResource>(It.IsAny<IRestRequest>()))
-                .Returns(response.Object);
-
-            mockIRestClient
-                .Setup(x => x.ExecuteTaskAsync<MyResource>(It.IsAny<IRestRequest>(), It.IsAny<CancellationToken>()))
-                .Returns(Task.FromResult(response.Object));
-
-            return new MyClient(apiKey)
-            {
-                RestClient = mockIRestClient.Object
-            };
+            return response;
         }
 
-        private MyClient GetErroredResponseClient()
+        private Mock<IRestResponse<MyResource>> ErroredResponse()
         {
             var response = new Mock<IRestResponse<MyResource>>();
             response.Setup(_ => _.StatusCode).Returns(System.Net.HttpStatusCode.Created);
@@ -150,51 +138,38 @@ namespace Recurly.Tests
             response.Setup(_ => _.ErrorException).Returns(new Exception("parsing error"));
             response.Setup(_ => _.ErrorMessage).Returns("parsing error");
 
-            var mockIRestClient = new Mock<IRestClient>();
-            mockIRestClient
-                .Setup(x => x.Execute<MyResource>(It.IsAny<IRestRequest>()))
-                .Returns(response.Object);
-
-            return new MyClient(apiKey)
-            {
-                RestClient = mockIRestClient.Object
-            };
+            return response;
         }
 
-        private MyClient CreateResourceSuccessClient()
-        {
-            var data = new MyResource()
-            {
-                MyString = "benjamin"
-            };
-            var response = new Mock<IRestResponse<MyResource>>();
-            response.Setup(_ => _.StatusCode).Returns(System.Net.HttpStatusCode.Created);
-            response.Setup(_ => _.Content).Returns("{\"my_string\": \"benjamin\"}");
-            response.Setup(_ => _.Headers).Returns(new List<Parameter> { });
-            response.Setup(_ => _.Data).Returns(data);
-
-            var mockIRestClient = new Mock<IRestClient>();
-            mockIRestClient
-                .Setup(x => x.Execute<MyResource>(It.IsAny<IRestRequest>()))
-                .Returns(response.Object);
-
-            return new MyClient(apiKey)
-            {
-                RestClient = mockIRestClient.Object
-            };
-        }
-
-        private MyClient GetResourceFailureClient()
+        private Mock<IRestResponse<MyResource>> NotFoundResponse()
         {
             var response = new Mock<IRestResponse<MyResource>>();
             response.Setup(_ => _.StatusCode).Returns(System.Net.HttpStatusCode.NotFound);
             response.Setup(_ => _.Content).Returns("{\"error\":{ \"type\": \"not_found\", \"message\": \"MyResource not found\"}}");
             response.Setup(_ => _.Headers).Returns(new List<Parameter> { });
 
+            return response;
+        }
+
+        private MyClient MockResourceClient(Mock<IRestResponse<MyResource>> response)
+        {
+            Func<IRestRequest, bool> matcher = delegate (IRestRequest request)
+            {
+                return true;
+            };
+            return MockResourceClient(matcher, response);
+        }
+
+        private MyClient MockResourceClient(Func<IRestRequest, bool> matcher, Mock<IRestResponse<MyResource>> response)
+        {
             var mockIRestClient = new Mock<IRestClient>();
             mockIRestClient
-                .Setup(x => x.Execute<MyResource>(It.IsAny<IRestRequest>()))
+                .Setup(x => x.Execute<MyResource>(It.Is<RestRequest>(r => matcher(r))))
                 .Returns(response.Object);
+
+            mockIRestClient
+                .Setup(x => x.ExecuteTaskAsync<MyResource>(It.Is<IRestRequest>(r => matcher(r)), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(response.Object));
 
             return new MyClient(apiKey)
             {
