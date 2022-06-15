c833742c4efb3e0c2a854f815652a0721f1e0b34
DX Codefresh <dx@recurly.com>
2022-06-15 22:47:49 +0000

    Generated Latest Changes for v2019-10-10

--- Recurly/Resources/SubscriptionChange.cs
+++ Recurly/Resources/SubscriptionChange.cs
@@ -79,7 +79,7 @@ namespace Recurly.Resources
         [JsonProperty("subscription_id")]
         public string SubscriptionId { get; set; }
 
-        /// <value>Determines whether or not tax is included in the unit amount. The Tax Inclusive Pricing feature (separate from the Mixed Tax Pricing feature) must be enabled to use this flag.</value>
+        /// <value>This field is deprecated. Please do not use it.</value>
         [JsonProperty("tax_inclusive")]
         public bool? TaxInclusive { get; set; }
 
