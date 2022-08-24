348a2698fd039b9ea962d5dbc5d321a28dca0896
DX Codefresh <dx@recurly.com>
2022-08-24 15:30:41 +0000

    Generated Latest Changes for v2021-02-25

---
+++ Recurly/Resources/ErrorMayHaveCVV.cs
@@ -0,0 +1,28 @@
+/**
+ * This file is automatically created by Recurly's OpenAPI generation process
+ * and thus any edits you make by hand will be lost. If you wish to make a
+ * change to this file, please create a Github issue explaining the changes you
+ * need and we will usher them to the appropriate places.
+ */
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Newtonsoft.Json;
+
+namespace Recurly.Resources
+{
+    [ExcludeFromCodeCoverage]
+    public class ErrorMayHaveCVV : Resource
+    {
+
+        /// <value>The security code you entered does not match. Please update the CVV and try again.</value>
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        /// <value>Type</value>
+        [JsonProperty("type")]
+        [JsonConverter(typeof(RecurlyStringEnumConverter))]
+        public Constants.ErrorType? Type { get; set; }
+
+    }
+}
