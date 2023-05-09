877c945b044c99b7f5f72be9f8a2ae7ef0c9b149
sunny <swong@recurly.com>
2023-05-09 14:19:20 -0400

    Updates External Sub UUID Test

--- Test/ExternalSubscriptionTest.cs
+++ Test/ExternalSubscriptionTest.cs
@@ -10,7 +10,7 @@ namespace Recurly.Test
         [RecurlyFact(TestEnvironment.Type.Integration)]
         public void LookupExternalSubscription()
         {
-            var uuid = "rv1loyqlf6si";
+            var uuid = "sv3fm556opc8";
             var externalSubscription = ExternalSubscriptions.Get(uuid);
             externalSubscription.Quantity.Should().Be(14);
             externalSubscription.State.Should().Be("");
