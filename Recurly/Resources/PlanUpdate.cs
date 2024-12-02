ad2f007e4ae3b733012a9c244c1e9c522ee12003
Recurly Integrations <dx@recurly.com>
2024-12-02 18:35:12 +0000

    Generated Latest Changes for v2021-02-25

--- Recurly/Resources/PlanUpdate.cs
+++ Recurly/Resources/PlanUpdate.cs
@@ -162,5 +162,9 @@ namespace Recurly.Resources
         [JsonConverter(typeof(RecurlyStringEnumConverter))]
         public Constants.IntervalUnit? TrialUnit { get; set; }
 
+        /// <value>Used by Vertex for tax calculations. Possible values are `sale`, `rental`, `lease`.</value>
+        [JsonProperty("vertex_transaction_type")]
+        public string VertexTransactionType { get; set; }
+
     }
 }
