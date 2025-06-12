17f00f5e6486656044880e061e041fdbf7553a7f
Eugene Bekker <ebekker@users.noreply.github.com>
2025-06-12 18:06:47 -0400

    adding support for Webhook Signature Verification

---
+++ Recurly.Tests/WebhooksTests.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Xunit;
+
+namespace Recurly.Tests
+{
+    public class WebhooksTests
+    {
+        public const string Secret = "354fab5bd35f5a0d50845ee7e30165244468b13c2c343f104e4e730a59326d9d";
+        public const string SecretAlt = "244468b13c2c343f104e4e730a59326d9d354fab5bd35f5a0d50845ee7e30165";
+        public const string Body = "{\"id\":\"rjxwmwedwqug\",\"object_type\":\"account\",\"site_id\":\"qc326l1hl8k9\",\"event_type\":\"created\",\"event_time\":\"2022-09-13T21:18:40Z\",\"account_code\":\"adfas23zzz14123\"}";
+
+        public const string OldTimetamp = "1663103925004";
+        public const string ExpectedSignatureAtOldTimestamp = "ad24699f3beaa24c9af2cc7ada3ce4c835da0090194241b9d3add83d27f14bc3";
+        public const string ExpectedSignatureAltAtOldTimestamp = "FCE309484E6A8360250DF367CCA0BCFC9FFFDC420AEAC2D97B9ED4560D8E4485";
+
+        public static readonly string OldHeader = $"{OldTimetamp},{ExpectedSignatureAtOldTimestamp}";
+
+        [Fact]
+        public void ValidSignature()
+        {
+            var ts = DateTimeOffset.Now.ToUnixTimeMilliseconds() - 1000; // Jump back in time by 1s
+            var secret = Secret;
+            var header = ts.ToString();
+            using (var hmac = new HMACSHA256())
+            {
+                hmac.Key = Encoding.UTF8.GetBytes(secret);
+                header += "," + BitConverter.ToString(hmac.ComputeHash(
+                    Encoding.UTF8.GetBytes($"{ts}.{Body}"))).Replace("-", "");
+            }
+
+            Assert.True(Webhooks.VerifyWebhookSignature(header, secret, Body),
+                "signature should validate");
+            Assert.True(Webhooks.VerifyWebhookSignature(header, secret, Body,
+                throwOnFailure: true),
+                "signature should validate and not throw");
+        }
+        [Fact]
+        public void ValidSignatureAlt()
+        {
+            var ts = DateTimeOffset.Now.ToUnixTimeMilliseconds() - 1000; // Jump back in time by 1s
+            var secret = SecretAlt;
+            var header = ts.ToString();
+            using (var hmac = new HMACSHA256())
+            {
+                header += "," + ExpectedSignatureAtOldTimestamp;
+
+                hmac.Key = Encoding.UTF8.GetBytes(secret);
+                header += "," + BitConverter.ToString(hmac.ComputeHash(
+                    Encoding.UTF8.GetBytes($"{ts}.{Body}"))).Replace("-", "");
+
+                header += "," + ExpectedSignatureAltAtOldTimestamp;
+            }
+
+            Assert.True(Webhooks.VerifyWebhookSignature(header, secret, Body),
+                "signature should validate");
+            Assert.True(Webhooks.VerifyWebhookSignature(header, secret, Body,
+                throwOnFailure: true),
+                "signature should validate and not throw");
+        }
+
+        /// <summary>
+        /// Tests just the signature computation and comparison, ignoring
+        /// the timestamp component by specifying a huge tolerance.
+        /// </summary>
+        [Fact]
+        public void ValidSignatureIgnoringTimestamp()
+        {
+            var header = OldHeader;
+            var secret = Secret;
+            var tolerance = long.MaxValue;
+
+            Assert.True(Webhooks.VerifyWebhookSignature(header, secret, Body,
+                tolerance: tolerance),
+                "signature should validate");
+            Assert.True(Webhooks.VerifyWebhookSignature(header, secret, Body,
+                tolerance: tolerance,
+                throwOnFailure: true),
+                "signature should validate and not throw");
+        }
+        [Fact]
+        public void ValidSignatureAltIgnoringTimestamp()
+        {
+            var header = $"{OldHeader},{ExpectedSignatureAltAtOldTimestamp}";
+            var secret = SecretAlt;
+
+            Assert.True(Webhooks.VerifyWebhookSignature(header, secret, Body,
+                tolerance: long.MaxValue),
+                "signature should validate");
+            Assert.True(Webhooks.VerifyWebhookSignature(header, secret, Body,
+                tolerance: long.MaxValue,
+                throwOnFailure: true),
+                "signature should validate and not throw");
+        }
+
+        [Fact]
+        public void InvalidTimestamp()
+        {
+            var header = OldHeader;
+            var secret = Secret;
+
+            // Test with and without throwOnFailure
+            Assert.False(Webhooks.VerifyWebhookSignature(header, secret, Body),
+                "signature verification should fail");
+            
+            var ex = Assert.Throws<InvalidOperationException>(() =>
+                Webhooks.VerifyWebhookSignature(header, secret, Body,
+                    throwOnFailure: true));
+            Assert.False(ex.Data.Contains("expected"),
+                "exception should not contain `expected` data");
+            Assert.False(ex.Data.Contains("actual"),
+                "exception should not contain `actual` data");
+        }
+
+        [Fact]
+        public void InvalidSignature()
+        {
+            var ts = DateTimeOffset.Now.ToUnixTimeMilliseconds() - 1000; // Jump back in time by 1s
+            var secret = Secret;
+            var header = ts.ToString();
+            using (var hmac = new HMACSHA256())
+            {
+                hmac.Key = Encoding.UTF8.GetBytes(secret);
+                var badSig = new string((BitConverter.ToString(hmac.ComputeHash(
+                    Encoding.UTF8.GetBytes($"{ts}.{Body}"))).Replace("-", ""))
+                    .Reverse()
+                    .ToArray());
+                header += "," + badSig;
+            }
+
+            Assert.False(Webhooks.VerifyWebhookSignature(header, secret, Body),
+                "signature should not validate");
+            var ex = Assert.Throws<InvalidOperationException>(() =>
+                Webhooks.VerifyWebhookSignature(header, secret, Body,
+                    throwOnFailure: true));
+            Assert.True(ex.Data.Contains("expected"),
+                "exception should contain `expected` data");
+            Assert.True(ex.Data.Contains("actual"),
+                "exception should contain `actual` data");
+        }
+
+        [Fact]
+        public void InvalidSecretKey()
+        {
+            var header = OldHeader;
+            var secret = SecretAlt;
+
+            // Test with and without throwOnFailure
+            Assert.False(Webhooks.VerifyWebhookSignature(header, secret, Body,
+                tolerance: long.MaxValue));
+            
+            var ex = Assert.Throws<InvalidOperationException>(() =>
+                Webhooks.VerifyWebhookSignature(header, secret, Body,
+                    tolerance: long.MaxValue,
+                    throwOnFailure: true));
+            Assert.True(ex.Data.Contains("expected"),
+                "exception should contain `expected` data");
+            Assert.True(ex.Data.Contains("actual"),
+                "exception should contain `actual` data");
+        }
+    }
+}
---
+++ Recurly/Webhooks.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Recurly
+{
+    public static class Webhooks
+    {
+        /// <summary>
+        /// Default signature timestamp difference tolerance in milliseconds.
+        /// </summary>
+        public const long DefaultTolerance = 5 * 60 * 1000;
+        public static readonly byte PeriodByte = Convert.ToByte('.');
+
+        internal const string WebhookErrParsing = "webhook signature header parsing failed";
+	    internal const string WebhookErrToleranceExceeded = "webhook tolerance exceeded";
+	    internal const string WebhookErrSignatureMismatch = "webhook signatures do not match";
+        private static readonly char[] HeaderSplitChars = new[] { ',' };
+
+        /// <summary>
+        /// Verifies the header-provided signature for a JSON Webhook payload.
+        /// </summary>
+        /// <remarks>
+        /// Recurly webhook signature verification is based on
+        /// <see href="https://docs.recurly.com/v1.3/docs/signature-verification"
+        ///     >this documentation</see>.
+        /// </remarks>
+        /// <param name="header">value of the <code>recurly-signature</code></param>
+        /// <param name="secret">endpoint-specific secret key of a configured webhook</param>
+        /// <param name="body">complete, raw HTTP body of the Webhook payload</param>
+        /// <param name="tolerance">difference tolerance between current timestamp and
+        ///     header-provided timestamp</param>
+        /// <param name="throwOnFailure">if true, then an exception will be thrown
+        ///     if the signature fails validation for any reason, defaults to false</param>
+        /// <returns>true if the validation succeeds, false if the validation fails for any
+        ///     reason and <code>throwOnFailure</code> parameter is false</returns>
+        public static bool VerifyWebhookSignature(string header, string secret, string body,
+            long tolerance = DefaultTolerance,
+            bool throwOnFailure = false)
+        {
+            var secretBytes = Encoding.UTF8.GetBytes(secret);
+            var headerParts = header.Split(HeaderSplitChars, 2);
+            if (headerParts.Length < 2)
+            {
+                return throwOnFailure
+                    ? throw new FormatException(WebhookErrParsing)
+                    : false;
+            }
+
+            var headerTimestamp = headerParts[0];
+            var headerSigs = headerParts[1].Split(HeaderSplitChars);
+            if (!long.TryParse(headerTimestamp, out var sigUnixTimestamp))
+            {
+                return throwOnFailure
+                    ? throw new FormatException(WebhookErrParsing)
+                    : false;
+            }
+
+            var sigTimestamp = DateTimeOffset.FromUnixTimeMilliseconds(sigUnixTimestamp);
+            var curTimestamp = DateTimeOffset.UtcNow;
+            if ((curTimestamp - sigTimestamp).TotalMilliseconds > tolerance)
+            {
+                return throwOnFailure
+                    ? throw new InvalidOperationException(WebhookErrToleranceExceeded)
+                    : false;
+            }
+
+            var dataSize = headerTimestamp.Length + 1 + body.Length;
+            var dataBuffer = new byte[dataSize];
+
+            string hashHex = null;
+            using (var hmac = new HMACSHA256(secretBytes))
+            using (var stream = new MemoryStream(dataBuffer))
+            {
+                var tsBytes = Encoding.UTF8.GetBytes(headerTimestamp);
+                var bodyBytes = Encoding.UTF8.GetBytes(body);
+
+                stream.Write(tsBytes, 0, tsBytes.Length);
+                stream.WriteByte(PeriodByte);
+                stream.Write(bodyBytes, 0, bodyBytes.Length);
+
+                stream.Seek(0, SeekOrigin.Begin);
+                var hashBytes = hmac.ComputeHash(stream);
+                hashHex = BitConverter.ToString(hashBytes).Replace("-", "");
+            }
+
+            for (var i = 0; i < headerSigs.Length; i++)
+            {
+                if (string.Equals(hashHex, headerSigs[i], StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+
+            return throwOnFailure
+                ? throw new InvalidOperationException(WebhookErrSignatureMismatch + ";" + hashHex)
+                {
+                    Data =
+                    {
+                        ["expected"] = headerParts[1],
+                        ["actual"] = hashHex,
+                    },
+                }
+                : false;
+        }
+    }
+}
